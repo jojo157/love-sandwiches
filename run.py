f359934cb9c08fde6b39e18f2ba993657c538987
jojo157 <carrigj@tcd.ie>
2021-05-09 18:27:19 +0000

    added function to get stock value predictions and the sandwich type and return this

--- run.py
+++ run.py
@@ -114,7 +114,17 @@ def main():
     sales_columns= get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 print("Welcome to Love Sandwiches Data automation")
-main()
+stock_data = main()
 
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").get_all_values()
+    headings = headings[0]
+    dict_stock = dict(zip(headings, data))
+    
+    return dict_stock
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
